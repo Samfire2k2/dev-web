71a473e69f5bd72c5d87640eabf2690911ba9bb7
Alex R <alexrioux44@gmail.com>
2023-03-22 08:49:01 +0100

    Crud

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('adresseDep')
+            ->add('Destination')
+            ->add('dateHeureDep')
+            ->add('nbPlacesDispo')
+            ->add('prix')
+            ->add('conducteur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}

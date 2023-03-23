94031f9174155904da082eec9b3568fc15ebb8f6
Alex R <alexrioux44@gmail.com>
2023-03-23 13:12:45 +0100

    nav-bar

--- src/DataFixtures/PassagerFixtures.php
+++ src/DataFixtures/PassagerFixtures.php
@@ -19,9 +19,9 @@ class PassagerFixtures extends Fixture
         $faker = Faker::create('fr_FR');
         $passager1 = new Passager();
         $passager1
-        ->setEmail("")
-        ->setPassword("");
-        //->setRoles()
+        ->setEmail("alexrioux44@gmail.com")
+        ->setPassword('$argon2id$v=19$m=65536,t=4,p=1$L3c5MlNIOGdTUEttNUd0NQ$EYGjwC5KkwMZQEW2WiWzPhvJxbeW5vubdmSqZvpBdIs')
+        ->setRoles(["ROLE_ADMIN"]);
         $manager->persist($passager1);
 
         $manager->flush();
